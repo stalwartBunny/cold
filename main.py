3578336959113f72abe17c60358ae83c9310e4d4
stalwartBunny <constructingintelligence@gmail.com>
2022-08-26 21:01:30 -0500

    started mocking up the loop engine

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ slotC = []
 slotD = []
 lockedLoops = []
 turnCount = 0
-deckA = []
-deckB = []
 terrainStack = []
 
 class Card:
@@ -43,19 +41,82 @@ class Card:
 
 
 class cardSlot:
+    loopNumber = 1
     player1Cards = []
     player2Cards = []
     loopStack = []
     player1Outcome = []
     player2Outcome = []
+    terrain = "None"
 
+    def __init__(self, player1Cards, player2Cards):
+        self.player1Cards = player1Cards
+        self.player2Cards = player2Cards
+
+    def func(self):
+        print("After calling the func method...")
+        print(f"This loop is numbered {loopNumber}, contains {player1Cards} from player 1, {player2Cards} from player 2, has a stack of {loopStack}, a player 1 outcome of {player1Outcome}, a player 2 outcome of {player2Outcome}, and {terrain} as terrain.")
+        print("....function end....")
+
+
+Spark = Card("Spark", "Red", "None", 1, 0, 0, "None")
 LightningBolt = Card("Lightning Bolt", "Yellow", "None", 3, 0, 0, "None")
 FluidCounter = Card("Fluid Counter", "Blue", "None", 0, 3, 0, "None")
+Nudge = Card("Nudge", "Green", "None", 0, 1, 0, "None")
 LightningBolt.func()
 FluidCounter.func()
-print(LightningBolt.name)
+Spark.func()
+Nudge.func()
+
+def draw():
+    global hand1
+    global hand2
+
+    seed1 = random.randint(1,30)
+    seed2 = random.randint(1,30)
+    seed3 = random.randint(1,30)
+    seed4 = random.randint(1,30)
+    seed5 = random.randint(1,30)
+    seed6 = random.randint(1,30)
+    seed7 = random.randint(1,30)
+    seed8 = random.randint(1,30)
+
+    hand1 = [LightningBolt, LightningBolt, LightningBolt, Spark]
+    hand2 = [FluidCounter, FluidCounter, Nudge, Nudge]
+#hard coded hands for now
+
+
+
+def submitA():
+    handCodeA = input("Submit your card selections in order for player A (1243) adding * to lock a loop instead (12*3)>> ")
+    if handCodeA.length != 4:
+        print("Something seems to be wrong with your submission, try again.")
+        submitA()
+
+def submitB():
+    handCodeB = input("Submit your card selections in order for player B (1243) adding * to lock a loop instead (12*3)>> ")
+    if handCodeB.length != 4:
+        print("Something seems to be wrong with your submission, try again.")
+        submitB()
+
+def calculateLoops():
+    slotA = cardSlot(handCodeA[0], handCodeB[0])
+    slotB = cardSlot(handCodeA[1], handCodeB[1])
+    slotC = cardSlot(handCodeA[2], handCodeB[2])
+    slotD = cardSlot(handCodeA[3], handCodeB[3])
+    if handCodeA[0] != "*":
+        #to do, loop calculation
+
+
 
 def main():
+    deckA = [LightningBolt, LightningBolt, LightningBolt, LightningBolt]
+    deckALive = []
+    deckB = [FluidCounter, Nudge, FluidCounter, Nudge]
+    deckBLive = []
+    handCodeA = 0
+    handCodeB = 0
+
     print(f"Welcome to Creatures of Light and Darkness. Submit cards to slots left-to-right and by their listing in your hand.")
     print("For example, a hand with [Lightning Bolt], [Lightning Bolt], [Fluid Counter], [Fluid Counter] submitted as 1234 puts bolt in slots 1 and 2, and Counter in 3 and 4.")
     print("Whereas 1324 submits as Bolt, Counter, Bolt, Counter.")
@@ -63,9 +124,9 @@ def main():
 
 
     while hp1 > 0 and hp2 > 0:
-        draw() #to do
-        submitA() #to do
-        submitB() #to do
+        draw() #to do (correctly, current setup hardcodes hands)
+        submitA()
+        submitB()
         calculateLoops() #to do
 
     if hp1 <= 0:
